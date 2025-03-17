efc1ce810bf46e555f68da3e77de6b5b939b58a6
MIchael Shane Reilly Marulanda <shane-reilly1@hotmail.com>
2025-03-17 20:58:50 +0000

    Vite Implementation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,42 +1,20 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const path = require('path');
 
+
+/** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: [
+  "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  addons: [
-    "@storybook/addon-webpack5-compiler-swc",
+  "addons": [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions"
   ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
-  },
-  webpackFinal: async (config) => {
-    // Add SCSS support
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-    });
-    
-    // Remove any existing SVGR rule if present
-    config.module.rules = config.module.rules.filter(rule => {
-      return !(rule.test && rule.test.toString().includes('svg'));
-    });
-
-    // Add a rule to handle SVGs as asset resources (URL strings)
-    config.module.rules.push({
-      test: /\.svg$/,
-      type: 'asset/resource', // webpack 5 way to emit files and return their URL
-    });
-
-    return config;
-  },
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
 };
-
 export default config;
